013f8805cb0c0ea20a7ce45ecba4ff210f2cfd54
Jiogo18 <30299784+Jiogo18@users.noreply.github.com>
2018-09-29 22:21:58 +0200

    Create random.js

---
+++ .gitignore/commandes/random.js
@@ -0,0 +1,16 @@
+module.exports = class CmdRandom
+{
+	static action (message, msg)
+	{
+		switch (msg[0].toLowerCase())
+		{
+			case "ping":
+				message.channel.send('pong, '+message.author+" !")
+				break
+
+			case "id":
+				message.channel.send("ID de "+message.author+" : "+message.author.id)
+				break
+		}
+	}
+}
