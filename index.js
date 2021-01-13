118d3847e906d35b807f51d4dc8fc8e671142ea6
Jiogo18 <30299784+Jiogo18@users.noreply.github.com>
2021-01-13 22:56:57 +0100

    call a /commands with @Jig0ll

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();//id du bot:<@!494587865775341578>
 const Cmd = require("./commandes/commande.js");
 var messageNotCmd = [];
-const interactionManager = require('./interaction/handler.js');
-const interactionMgr = new interactionManager(bot);
+const InteractionManager = require('./interaction/handler.js');
+const interactionMgr = new InteractionManager(bot);
 require('colors');//colors for everyone ! (don't remove)
 
 
@@ -26,35 +26,44 @@ bot.on(Discord.Constants.Events.CLIENT_READY, () => {
 });
 
 
+
+
+function onMessageNotCommand(message) {
+	var sourceId = 0, sourceName = "Unknow";
+	if(message.guild) {
+		sourceId = message.guild.id;
+		sourceName = message.guild.name+"@"+sourceId+"/"+message.channel.name;
+	}
+	else if(message.author) {
+		sourceId = message.author.id;
+		sourceName = "MP^";
+	}
+
+	if(!messageNotCmd[sourceId])
+		messageNotCmd[sourceId] = 0;
+	if(++messageNotCmd[sourceId] >= 200) {//à 200
+		messageNotCmd[sourceId] = 0;
+		console.warn(`Plus de 200 messages dans ${sourceName}, laissez moi dormir`);
+	}
+}
+
+
 bot.on(Discord.Constants.Events.MESSAGE_CREATE, message => {
 	
-	if(!interactionMgr.config.isAllowed(
+	if(!InteractionManager.config.isAllowed(
 		{
 			user: message.author,//infos du message
 			guild: message.channel.guild,
-			channel: message.channel
-		}, false))
-		return;//pas autorisé
+			channel: message.channel,
+			on: 'message'
+		}, false)
+	) { return; }//pas autorisé
+
 
 	try {
 		var msg = Cmd.isCommand(bot, message);
 		if(!msg) {//if it's not a command
-			var sourceId = 0, sourceName = "Unknow";
-			if(message.guild) {
-				sourceId = message.guild.id;
-				sourceName = message.guild.name+"@"+sourceId+"/"+message.channel.name;
-			}
-			else if(message.author) {
-				sourceId = message.author.id;
-				sourceName = "MP^";
-			}
-
-			if(!messageNotCmd[sourceId])
-				messageNotCmd[sourceId] = 0;
-			if(++messageNotCmd[sourceId] >= 200) {//à 200
-				messageNotCmd[sourceId] = 0;
-				console.warn(`Plus de 200 messages dans ${sourceName}, laissez moi dormir`);
-			}
+			onMessageNotCommand(message);
 			return;
 		}
 		console.log(`nouvelle commande dans ${message.id} (par ${message.author.username}@${message.author.id}) : ${message.content}`);
@@ -66,7 +75,17 @@ bot.on(Discord.Constants.Events.MESSAGE_CREATE, message => {
 
 	//2 try catch to answer with the error ONLY when it's a command
 	try {
-		Cmd.action(bot, message, msg);
+		var args = [...msg];//copy
+		const commandName = args.shift();
+		const retour = interactionMgr.onCommand(args,
+			{ user: message.author, guild: message.channel.guild, channel: message.channel, on: 'message' });//TODO : fonction
+		
+		if(retour) {
+			message.channel.send(retour);
+			console.log('answer of message sent by interactionMgr'.red);
+		}
+		else
+			Cmd.action(bot, message, msg);
 	} catch (error) {
 		message.channel.send(`Sorry I've had an error: ${error}`);
 		console.error(error);
