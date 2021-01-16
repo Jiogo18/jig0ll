62e9c8c7c5bf26447cf0c2002c41e283d8a51686
Jiogo18 <30299784+Jiogo18@users.noreply.github.com>
2021-01-16 19:12:14 +0100

    added anonyme

---
+++ commands/anonyme.js
@@ -0,0 +1,33 @@
+const MessageMaker = require('../Interaction/messageMaker.js');
+
+
+module.exports = {
+
+	name: 'anonyme',
+	description: 'Faire une annonce anonymement',
+
+	interaction: false,
+	hidden: true,
+	public: true,
+	wip: true,
+
+
+
+	options: [{
+		name: 'message',
+		type: 3,
+		required: true,
+
+		execute(cmdData) {
+			var options = cmdData.optionsValue;
+
+			const message = options.join(' ');
+			
+			if(cmdData.commandSource) {
+				if(cmdData.commandSource.delete) cmdData.commandSource.delete();
+			}
+			return new MessageMaker.Embed('Anonyme', message, 3);
+		}
+	}]
+
+}
