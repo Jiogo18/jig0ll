3f7fa2f0e5c9d827f186c12c274d17bffb5ca22e
Jiogo18 <30299784+Jiogo18@users.noreply.github.com>
2021-01-27 00:13:08 +0100

    test des interactions Discord à changer

--- commands/debug.js
+++ commands/debug.js
@@ -58,8 +58,30 @@ module.exports = {
 			execute() { return done; },
 			options: [{
 				name: 'very_long', description: 'A very long command line', type: 3,
-				execute() { return done; }
+				execute(cmdData) { console.debug(cmdData.options); return done; }
+			},{
+				name: 'number', description: 'A command line with a number', type: 4,
+				execute(cmdData) { console.debug(cmdData.options); }
+			},{
+				name: 'string', description: 'A string', type: 3,
+				execute(cmdData) { console.debug(cmdData.options); }
+			},{
+				name: 'user', description: 'A user', type: 6,
+				execute(cmdData) { console.debug(cmdData.options); }
+			},{
+				name: 'bool', description: 'A boolean', type: 5,
+				execute(cmdData) { console.debug(cmdData.options); }
+			},{
+				name: 'role', description: 'A role', type: 8,
+				execute(cmdData) { console.debug(cmdData.options); }
 			}]
 		}]
+	},{
+		name: 'grouptest', description: 'group de test', type: 2,
+		options: [{
+			name: 'test', description: 'func test', type: 1,
+			execute() { return 'func test ok'; }
+		}],
+		execute() { return 'ok test'; }
 	}]
 };
