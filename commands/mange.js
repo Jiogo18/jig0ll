53646a080b9f857ba1bf01b7ee37d8c498473551
Jiogo18 <30299784+Jiogo18@users.noreply.github.com>
2021-02-03 00:10:41 +0100

    executeAttribute and interaction for mange

--- commands/mange.js
+++ commands/mange.js
@@ -1,4 +1,5 @@
 const MessageMaker = require('../lib/messageMaker.js');
+const Snowflake = require('../lib/snowflake.js');
 
 const logChannels = {
 	'313048977962565652': '801844472836784167',//serveur Jiogo #log-manger
@@ -9,8 +10,8 @@ module.exports = {
 
 
 	name: 'mange',
-	description: 'Donner un compte rendu de son repas inrp\nFormat: `!mange "Elizia" "des chocolats"`',
-	interaction: false,//FUTUR discord accepte pas encore de mettre 2 type 3 à la suite (ref?)
+	description: 'Donne un compte rendu de son repas inrp\nFormat: `!mange "Elizia" "des chocolats"`',
+	interaction: true,
 	
 	security: {
 		place: 'public',
@@ -21,33 +22,47 @@ module.exports = {
 		type: 6,//user
 		description: 'Qui mange ?',
 		required: true,
+	},{
+		name: 'quoi',
+		type: 3,
+		description: 'Manger quoi ?',
+		required: true,
+	}],
 
-		options: [{
-			name: 'quoi',
-			type: 3,
-			description: 'Manger quoi ?',
-
-			execute(cmdData) {
-				const guildId = cmdData.guild.id;
-				//le channel dans la liste logChannels ou le channel d'où le message est envoyé
-				const channelLog = logChannels[guildId] ? cmdData.bot.channels.cache.get(logChannels[guildId]) : undefined;
+	async executeAttribute(cmdData, levelOptions) {
+		if(levelOptions.length < 2) return new MessageMaker.Embed('Mange', module.exports.description);//n'a pas respecté les options
 
 
-				const sujet = cmdData.optionsValue[0];
-				const aliment = cmdData.optionsValue[1];
-				const channelSource = cmdData.channel;
-				const strDansChannel = (channelSource && channelSource.name) ? ` dans ${channelSource}` : ''
-				const retour = new MessageMaker.Embed('', `Aujourd'hui ${sujet} a mangé ${aliment}${strDansChannel}`);
-				if(channelLog) {
-					channelLog.send(retour.getForMessage());
-				}
-				else {
-					return retour;//répond au message
-				}
-				return new MessageMaker.Embed('', "Commande effectuée, bon appétit");
-			}
-		}],
+		const guildId = cmdData.guild.id;
+		//le channel dans la liste logChannels ou le channel d'où le message est envoyé
+		const channelLog = logChannels[guildId] ? cmdData.bot.channels.cache.get(logChannels[guildId]) : undefined;
 
-		execute() { return new MessageMaker.Message(module.exports.description); }
-	}]
+		const sujet = await getTarget(cmdData.bot, levelOptions.personne || levelOptions[0] && levelOptions[0].value);
+		const aliment = levelOptions.quoi || levelOptions[1] && levelOptions[1].value;
+		const channelSource = cmdData.channel;
+		const strDansChannel = (channelSource && channelSource.name) ? ` dans ${channelSource}` : ''
+		const retour = new MessageMaker.Embed('', `Aujourd'hui ${sujet} a mangé ${aliment}${strDansChannel}`);
+		if(channelLog) {
+			channelLog.send(retour.getForMessage());
+		}
+		else {
+			return retour;//répond au message
+		}
+		return new MessageMaker.Embed('', "Commande effectuée, bon appétit");
+	}
+}
+
+
+
+async function getTarget(bot, str)
+{
+	if(!Snowflake.isSnowflake(str)) return str;
+
+	const user = await bot.users.fetch(str);
+	if(user) {
+		if(user.partial == false) return user;//si on a l'user : target == user
+		return `<@${str}>`;//si on a un snowflake mais pas vraiment user : <@Snowflake>
+	}
+
+	return str;//si on a rien de tout ça : str
 }
