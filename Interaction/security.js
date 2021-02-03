5f111bdc1a8d96b4471761602e5016f42769a10a
Jiogo18 <30299784+Jiogo18@users.noreply.github.com>
2021-02-03 01:49:52 +0100

    remove old secuity things

--- Interaction/security.js
+++ Interaction/security.js
@@ -102,7 +102,6 @@ class SecurityCommand {
 			case SecurityPlace.NONE: return false;
 			case undefined: return false;
 			default:
-				//TODO: avoir une sécurité par héritage (créée au démarrage ?)
 				console.warn(`isAllowedToUse place unknow for ${this.name} : ${this.place}`);
 				return false;
 		}
@@ -154,46 +153,7 @@ module.exports = {
 	},
 
 
-	allowedPlace: SecurityPlace,
 	SecurityPlace: SecurityPlace,
-
-	isAllowedToCreateInteraction(command) {
-		if(command.interaction != true) { return this.allowedPlace.NONE; }
-		if(command.wip) { return this.allowedPlace.PRIVATE; }
-		if(command.public) { return this.allowedPlace.PUBLIC; }
-		if(command.private) { return this.allowedPlace.PRIVATE; }
-		console.warn(`${command.name} has no interaction place available`);
-		return this.allowedPlace.NONE;
-	},
-
-
-	isAllowedToUseCommand(command, context) {
-		if(command.wip && is.betaAllowed(context) == false) { return false; }
-
-		if(typeof command.isAllowed == 'function') { return command.isAllowed(context); }
-		if(typeof command.isAllowedToUse == 'function') { return command.isAllowedToUse(context); }
-		
-		if(command.private) { return is.hightPrivilegeUser(context.author.id); }
-		if(command.public) { return true; }
-		if(command.type > 0) { return true; }//autorisé (pour les sous commandes)
-		//TODO: avoir une sécurité par héritage (créée au démarrage ?)
-		console.warn(`isAllowedToUse unknow for ${command.name}`);
-	},
-
-	isAllowedToSeeCommand(command, context) {
-		if(typeof command.isAllowedToSee == 'function') { return command.isAllowedToSee(context); }
-		
-		if(command.secret || command.hidden) return false;
-		if(command.publicSee) return true;
-
-		return this.isAllowedToUseCommand(command, context);//sinon on l'affiche (si elle est disponible)
-	},
-
-	isAllowedToGetCommand(command, context, readOnly) {
-		if(readOnly) return this.isAllowedToSeeCommand(command, context);
-		return this.isAllowedToUseCommand(command, context);
-	},
-
 
 	SecurityCommand: SecurityCommand
 }
