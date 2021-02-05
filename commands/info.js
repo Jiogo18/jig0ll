6f16d7d1edaca5bdfc08c425c088ed0fe978de64
Jiogo18 <30299784+Jiogo18@users.noreply.github.com>
2021-02-05 10:17:09 +0100

    moved info command

--- commands/info.js
+++ commands/info.js
@@ -2,32 +2,6 @@ const MessageMaker = require('../lib/messageMaker');
 const SnowflakeLib = require('../lib/snowflake');
 const DateLib = require('../lib/date');
 
-function makeMessage(description) {
-	return new MessageMaker.Embed('Info', description);
-}
-
-function getInfo(targetTitle, target) {
-	const snowflake = new SnowflakeLib.Snowflake(target.id);
-	const time = snowflake.msecSinceDiscord;
-	const date = DateLib.getFrenchDate(snowflake.msecSinceEpoch);
-	//si target est pas chargé ça affiche [object Object]
-	const targetName = target.partial != false ? target.username : target;
-	const retour = makeMessage(
-		`${target.name || target.username ? `Informations ${targetTitle} ${targetName}` : ''}
-		Snowflake : ${target.id}
-		timestamp : ${snowflake.secSinceEpoch}
-		créé ${date}`);
-	
-	retour.addField('Snowflake',
-		`time : ${time} (${time.toString(2)})
-		worker : ${snowflake.worker}
-		pid : ${snowflake.pid}
-		increment : ${snowflake.increment}
-		(https://discord.js.org/#/docs/main/stable/typedef/Snowflake)`);
-	return retour;
-}
-
-
 module.exports = {
 
 	name: 'info',
@@ -80,7 +54,7 @@ module.exports = {
 			type: 8,
 			required: true,//il n'y a pas de role 'actuel'
 		}],
-		execute: executeInfoChannel,
+		executeAttribute: executeInfoRole,
 	},{
 		name: 'guild',
 		description: 'Date de création du serveur',
@@ -91,33 +65,68 @@ module.exports = {
 
 
 
+function makeMessage(description) {
+	return new MessageMaker.Embed('Info', description);
+}
+
+function getTargetName(target) {
+	if(typeof target != 'object') return target;
+	if(target.partial == false) return target;
+	if(target.username) return target.username;
+	return target;//sinon target.name éventuellement
+}
+
+function getInfo(targetTitle, target) {
+	const snowflake = new SnowflakeLib.Snowflake(target.id);
+	const time = snowflake.msecSinceDiscord;
+	const date = DateLib.getFrenchDate(snowflake.msecSinceEpoch);
+	//si target est pas chargé ça affiche [object Object]
+	
+	const targetName = getTargetName(target);
+	return makeMessage(
+		`${target.name || target.username ? `Informations ${targetTitle} ${targetName}` : ''}
+		Snowflake : ${target.id}
+		créé ${date}`);
+}
 
 
 
-function executeInfoUser(cmdData) {
-	const option = cmdData.options ? cmdData.options[1] : undefined;//TODO: change this (en "localOption")
-	const userId = option ? option.value : undefined;
+async function executeInfoUser(cmdData, levelOptions) {
+	const userId = levelOptions ? levelOptions.user || (levelOptions[0] && levelOptions[0].value) : undefined;
 
-	const user = userId ? cmdData.bot.users.cache.get(userId) : cmdData.author;
+	const user = await cmdData.bot.users.fetch(userId || cmdData.author.id);
 	if(!user) {
 		return makeMessage(`L'utilisateur ${userId} est introuvable`);
 	}
 
 	return getInfo("de l'utilisateur", user);
 }
-function executeInfoChannel(cmdData) {
-	const option = cmdData.options ? cmdData.options[1] : undefined;//TODO: change this (en "localOption")
-	const channelId = option ? option.value : undefined;
+async function executeInfoChannel(cmdData, levelOptions) {
+	const channelId = levelOptions ? levelOptions.channel || (levelOptions[0] && levelOptions[0].value) : undefined;
 
-	const channel = channelId ? cmdData.guild.channels.cache.get(channelId) : cmdData.channel;
+	const channel = await cmdData.guild.channels.resolve(channelId || cmdData.channel.id);
 	if(!channel) {
 		return makeMessage(`Le channel ${channelId} est introuvable`);
 	}
 
-	return getInfo("du channel", channel);
+	return getInfo("du channel", channel).addField('',
+		`Membres (minimum) : ${channel.members.array().length}`);
+}
+async function executeInfoRole(cmdData, levelOptions) {
+	const roleId = levelOptions.role || (levelOptions[0] && levelOptions[0].value);
+
+	const role = roleId ? await cmdData.guild.roles.fetch(roleId) : undefined;
+	if(!role) {
+		return makeMessage(`Le role ${roleId} est introuvable`);
+	}
+
+	return getInfo("du role", role).addField('',
+		`Membres (minimum) : ${role.members.array().length}`);
 }
 function executeInfoGuild(cmdData) {
-	return getInfo("du serveur", cmdData.guild);
+	const guild = cmdData.guild;
+	return getInfo("du serveur", guild).addField('',
+		`Membres : ${guild.memberCount}`);
 }
 
 
