f3e413168a9726a866e190390b093caf73db1728
Jiogo18 <30299784+Jiogo18@users.noreply.github.com>
2021-03-02 02:43:12 +0100

    More invit commands

--- commands/miscellaneous/invit.js
+++ commands/miscellaneous/invit.js
@@ -1,4 +1,4 @@
-import { Guild } from 'discord.js';
+import { Collection, Guild, User } from 'discord.js';
 import { ReceivedCommand } from '../../bot/command/received.js';
 import { EmbedMaker } from '../../lib/messageMaker.js';
 
@@ -44,6 +44,38 @@ export default {
 		 * @param {ReceivedCommand} cmdData 
 		 */
 		async execute(cmdData) { return list(cmdData.guild) },
+	}, {
+		name: 'per_user',
+		description: "Compte les membres invités par chaque utilisateur",
+		type: 1,
+
+		options: [{
+			name: 'guild',
+			description: 'Serveur ciblé',
+			type: 3,
+		}],
+
+		/**
+		 * '!invit per_user <guildid|guildname>'
+		 * @param {ReceivedCommand} cmdData 
+		 * @param {[*]} levelOptions
+		 */
+		async executeAttribute(cmdData, levelOptions) {
+			const guild_id = levelOptions.guild || levelOptions[0]?.value;
+			if (!guild_id) return this.execute(cmdData);
+
+			const guilds = cmdData.bot.guilds.cache;
+			const guild = guilds.get(guild_id) || guilds.find(guild => guild.name == guild_id);
+			if (!guild) return makeError(`Impossible de trouver le serveur '${guild_id}'`);
+
+			return count_uses_per_user(guild, cmdData.guild);
+		},
+
+		/**
+		 * !invit per_user
+		 * @param {ReceivedCommand} cmdData 
+		 */
+		execute(cmdData) { return displayCountPerUser(cmdData.guild); }
 	}],
 }
 
@@ -74,10 +106,103 @@ async function list(guild) {
 		return makeError('Impossible de récupérer les invitations du serveur, la permission `Ggérer le serveur` doit être activée');
 	}
 
-	console.log(invites);
+	var invitesDesc = invites.map(i => `code:${i.code}, memberCount:${i.memberCount}, uses:${i.uses}, maxUses:${i.maxUses}, inviter:${i.inviter?.username}, createTimestamp:${i.createdTimestamp}`);
 
-	var invitesDesc = invites.map(i => `code:${i.code}, memberCount:${i.memberCount}, uses:${i.uses}, maxUses:${i.maxUses}, inviter:${i.inviter.username}, createTimestamp:${i.createdTimestamp}`);
+	if (invites.size == 0) {
+		return makeMessage(`No invits in ${guild.name}`);
+	}
+	return makeMessage([`Fetched ${invites.size} invites in ${guild.name} : `, ...invitesDesc].join('\n'));
+}
 
 
-	return makeMessage([`Fetched ${invites.size} invites : `, ...invitesDesc].join('\n'));
+
+/**
+ * Get invites of the guild
+ * @param {Guild} guild 
+ */
+export async function getInvites(guild) {
+	if (!guild) throw 'Vous devez être dans un serveur pour exécuter ceci';
+
+	try {
+		return await guild.fetchInvites();
+	} catch (err) {
+		console.error(`Error while getting invites of the guild ${guild.id}`.red, err);
+		throw 'Impossible de récupérer les invitations du serveur, la permission `Gérer le serveur` doit être activée';
+	}
+}
+
+
+/**
+ * Count and sort the number of uses of invitations with the user who created the invite
+ * @param {[{user: User, uses: number}]} invites 
+ */
+export async function countInvitesPerUserSorted(invites) {
+
+	if (!invites) return;
+
+	/**
+	 * Addition de toutes les invitations
+	 * @type {Collection<User,number>}
+	 */
+	const per_user = new Collection();
+	invites.forEach(i => {
+		const user = i.inviter;
+		per_user.set(user, i.uses + (per_user.get(user) || 0));
+	});
+
+
+	/**
+	 * Toutes les invitations triées
+	 * @type {[{user: User, uses: number}]}
+	 */
+	var invitesSorted = [];
+	per_user
+		.filter((v, k) => k != null)
+		.forEach((uses, user) => invitesSorted.push({ user, uses }));
+	invitesSorted = invitesSorted.sort((a, b) => (a.uses > b.uses ? -1 : a.uses < b.uses));
+	
+	return invitesSorted; 
+}
+
+
+/**
+ * Create a readable array of invites infos
+ * @param {[{user: User, uses: number}]} invites The invites to display
+ */
+export async function embedInvitesList(invites, currentGuild) {
+	/**
+	 * @type {Collection<string,GiuldMember>}
+	 */
+	const membersInGuild = await currentGuild?.members.fetch(invites.map(({ user }) => user))
+		.catch(console.error);
+	
+	return invites.map(({ user, uses }) => {
+		var userInGuild = user?.id && (membersInGuild?.has(user?.id) || currentGuild?.members.cache.get(user?.id));
+
+		const username = user && ((userInGuild ? user.toString?.() : undefined) || user.username || user.id);
+		return `${username} a invité ${uses} personnes`;
+	});
+}
+
+
+/**
+ * Display a list with the number of uses of invitations for each user
+ * @param {Guild} guild 
+ * @param {Guild} currentGuild
+ */
+async function displayCountPerUser(guild, currentGuild) {
+	var invitesSorted;
+	try {
+		const invites = await getInvites(guild);
+		invitesSorted = await countInvitesPerUserSorted(invites);
+		if (!invitesSorted?.length) {
+			return makeMessage(`Il n'y a pas d'invitations dans ${guild.name}`);
+		}
+	} catch (err) {
+		console.error(`Error with countInvitesPerUserSorted`.red, err);
+		return makeError(err);
+	}
+
+	const countDesc = await embedInvitesList(invitesSorted, currentGuild);
+	return makeMessage([`Il y a ${invitesSorted.length} invitations dans ${guild.name} : `, ...countDesc].join('\n'));
 }
--- commands/plenitude/plenitude.js
+++ commands/plenitude/plenitude.js
@@ -2,7 +2,11 @@ import { TemporaryKVDatabase } from '../../lib/database.js';
 import { EmbedMaker } from '../../lib/messageMaker.js';
 import { getFrenchDate } from '../../lib/date.js';
 import { sendWeatherRequest } from '../meteo.js';
-var kvPlenitude = new TemporaryKVDatabase(undefined, 'plenitude', undefined, 10000);
+import { countInvitesPerUserSorted, embedInvitesList, getInvites } from '../miscellaneous/invit.js';
+import { CommandContext, ReceivedCommand } from '../../bot/command/received.js';
+import { Guild, User } from 'discord.js';
+var kvPlenitude = new TemporaryKVDatabase(undefined, 'plenitude', { createTable: true, insertIfNotExist: true }, 10000);
+var kvInvite = new TemporaryKVDatabase(undefined, 'plenitude_invite', { createTable: true, insertIfNotExist: true }, 10000);
 
 const PlenWeekdays=["Primidi","Duodi","Tridi","Quartidi","Quintidi","Sextidi","Septidi"];
 const PlenMonths=["Pluviôse","Ventôse","Germinal","Floréal","Prairial","Messidor","Thermidor","Fructidor","Vendémiaire","Brumaire","Frimaire","Nivôse"];
@@ -13,13 +17,13 @@ const PlenCity = {
 
 	/**
 	 * Get the location of Plénitude
-	 * @returns {string} The current location
+	 * @returns {Promise<string>} The current location
 	 */
 	get: async function () { return await this.value.get() || 'Chamonix-Mont-Blanc'; },
 	/**
 	 * Change the location of Plénitude
 	 * @param {string} location Where you want to move
-	 * @returns {string} The new location
+	 * @returns {Promise<string>} The new location
 	 */
 	set: async function (location) {
 		await this.value.set(location);
@@ -58,6 +62,29 @@ export default {
 		description: 'Informations sur Plénitude',
 		type: 1,
 		execute: getInfo
+	}, {
+		name: 'invite_score',
+		description: "Affiche le score des invitations",
+		type: 1,
+		execute: getInviteScore
+	}, {
+		name: 'invite_end',
+		description: "Affiche le score des invitations",
+		type: 1,
+			security: {
+			/**
+			 * @param {CommandContext} context The context where you want to use this
+			 */
+			isAllowedToUse(context) {
+					const user = context.guild?.members?.cache.get(context.author?.id);//si on veut passer en fetch il faut TOUT passer en async...
+					//donc dans cette situation là il faut que l'user envoie au un message avant
+					if (!user) return false;
+					const userRole = user.roles?.cache;
+					//Maître du Jeu || admin || Jiogo
+					return userRole?.has('626121766061867020') || userRole?.has('652843400646885376') || userRole?.has('816090157207650355');
+			}
+		},
+		execute: closeInvitesCompetition
 	}],
 
 	execute: getInfo,
@@ -65,13 +92,27 @@ export default {
 	getMeteo,
 	getLocation,
 	setLocation,
-	setBot: bot => kvPlenitude.setDatabase(bot.database),
+	setBot: bot => {
+		kvPlenitude.setDatabase(bot.database);
+		kvInvite.setDatabase(bot.database);
+	},
 }
+
+
+
+function makeMessage(description) {
+	return new EmbedMaker('Plénitude', description);
+}
+function makeError(description) {
+	return new EmbedMaker('Plénitude', description,  { color: 'red' });
+}
+
+
 
 
 /**
  * Get the meteo at Plénitude
- * @returns {EmbedMaker} The meteo in an embed message
+ * @returns The meteo in an embed message
  */
 export async function getMeteo() {
 	return sendWeatherRequest(await PlenCity.get(), onWeatherPlenitude);
@@ -89,5 +130,140 @@ function onWeatherPlenitude(data) {
  * Get the generic info of Plénitude
  */
 function getInfo() {
-	return new EmbedMaker('Plénitude', "Plénitude est un lieu fictif avec un climat tropical");
+	return makeMessage(`La ville de Plénitude reste un lieu fictif.
+	Seule ville de Zemlji et par conséquent sa capitale, c'est une cité coloniale en pleine expansion où l'esclavage y est devenu une nécessité.
+	L'île en elle-même présente un climat tempéré.`);
+}
+
+
+
+
+/**
+ * Get the invites of the mounth per user
+ * @param {Guild} guild The guild with the invites
+ * @param {number} maxInvits The number of invites to display
+ */
+async function getCurrentInviteList(guild, maxInvits = -1) {
+	var invites = await getInvites(guild);
+	if (!invites) return;
+	//remove the old count from the invite (uses since the competition has started)
+	await Promise.all(invites.map(async i => i.uses -= (await kvInvite.get(i.code)) || 0));
+	
+	const invitesSorted = countInvitesPerUserSorted(invites);
+	if (maxInvits > -1) invitesSorted = invitesSorted.slice(0, maxInvits);
+	return invitesSorted;
+}
+
+/**
+ * Count the number of uses of invites
+ * @param {[{user: User, uses: number}]} invites
+ */
+function countInvitedMembers(invites) {
+	var count = 0;
+	invites.forEach(i => count += i.uses);
+	return count;
+}
+
+
+/**
+ * Get the number of invites of the month per user
+ * @param {ReceivedCommand} cmdData
+ */
+async function getInviteScore(cmdData) {
+	//const guild = cmdData.guild;
+	const guild = cmdData.bot.guilds.cache.get('626121178163183628');
+
+	/**
+	 * Number of uses of invites created by an user since the copetition has started
+	 * @type {[{user: User, uses: number}]}
+	 */
+	var invitesSorted;
+	try {
+		invitesSorted = await getCurrentInviteList(guild);
+		if (!invitesSorted?.length) {
+			return makeMessage(`Il n'y a pas d'invitations dans ${guild.name}`);
+		}
+	}
+	catch (err) {
+		console.error(`Error with getCurrentInviteList`.red, err);
+		return makeError(err);
+	}
+	
+
+	var countNewInvit = countInvitedMembers(invitesSorted);
+	const countDesc = await embedInvitesList(invitesSorted, cmdData.guild);
+	const dateResetmsec = parseInt(await kvPlenitude.get('InvitResetTime') || 0) || 0;
+	const dateReset = getFrenchDate(new Date(dateResetmsec));
+	return makeMessage([`Il y a ${countNewInvit} nouveaux membres depuis ${dateReset} :`, ...countDesc].join('\n'));
+}
+
+/**
+ * Is the user can be in the competition
+ * @param {User} user
+ */
+function isInvitesAllowedUser(user) {
+	return ! ['344183028366442497', '302050872383242240'].includes(user?.id);
+}
+
+/**
+ * Close the competition and get the winner of the month
+ * @param {ReceivedCommand} cmdData
+ */
+async function closeInvitesCompetition(cmdData) {
+	//const guild = cmdData.guild;
+	const guild = cmdData.bot.guilds.cache.get('626121178163183628');
+
+	/**
+	 * Number of uses of invites created by an user since the copetition has started
+	 * @type {[{user: User, uses: number}]}
+	 */
+	var invitesSorted;
+	try {
+		invitesSorted = await getCurrentInviteList(guild);
+		if (!invitesSorted?.length) {
+			return makeMessage(`Il n'y a pas d'invitations sur ce serveur`);
+		}
+	}
+	catch (err) {
+		return makeError(err);
+	}
+
+	const dateResetmsec = parseInt(await kvPlenitude.get('InvitResetTime') || 0) || 0;
+	const dateReset = getFrenchDate(new Date(dateResetmsec));
+
+
+
+	var winners = invitesSorted.filter(i => isInvitesAllowedUser(i.user));
+	
+	const bestScore = winners?.[0]?.uses;
+	if (bestScore == 0) {// si le meilleur score est 0 alors personne n'a gagné
+		return makeMessage(`Il n'y a pas de gagnant ce mois-ci, il n'y a pas eu de nouveau membre depuis ${dateReset}.`);
+	}
+	winners = winners.filter(i => i.uses == bestScore);
+
+
+	var winnerStr;
+	switch (winners?.length) {
+		case undefined:
+		case 0:
+			winnerStr = `Il n'y a pas de gagnant ce mois-ci.`;
+			break;
+		case 1:
+			winnerStr = `Ce mois-ci le gagnant est ${winners[0]?.user?.toString()} (${winners[0]?.user?.username}) !`;
+			break;
+		default:
+			winnerStr = `Ce mois-ci nous avons ${winners.length} gagnants : ${winners.join(', ')} !`;
+			break;
+	}
+	
+	
+	//and set the current uses in the database
+	const invites = await getInvites(guild);
+	await Promise.all(invites.map(async i => await kvInvite.set(i.code, i.uses)));
+	
+	kvPlenitude.set('InvitResetTime', Date.now());
+	
+	
+	const countDesc = await embedInvitesList(invitesSorted.splice(0, 5), cmdData.guild);
+	return makeMessage([`${winnerStr}\n${countInvitedMembers(invitesSorted)} nouveaux membres ont rejoints le serveur\n\nLes 5 premiers sont :`, ...countDesc].join('\n'));
 }
